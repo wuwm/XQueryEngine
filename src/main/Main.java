ba579e9a1f0cbc95c1d99b329c9389b370a8c6d3
Weiming Wu <wwm1994@gmail.com>
2018-01-28 02:16:02 -0800

    Complete the first version! Passed all the test provided on piazza.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,39 @@
 package main;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import xmlparser.XMLWriter;
+import xpath.autogenerate.*;
+import xpath.impl.XPathVisitor_232;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.util.LinkedList;
 
 public class Main {
+    public static void main(String[] args){
+        CharStream input = null;
+        try {
+//            input = CharStreams.fromFileName(" ");
+            input = CharStreams.fromString("doc(\"j_caesar.xml\")//(ACT,PERSONAE)/TITLE");//, 1);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        XPathLexer lexer = new XPathLexer(input);
+        XPathParser parser = new XPathParser(new CommonTokenStream(lexer));
+        XPathVisitor_232 visitor = new XPathVisitor_232();
+
+        LinkedList res = visitor.visit(parser.ap());
+        XMLWriter writer = new XMLWriter();
+        writer.output(res);
+    }
 }
--- src/xpath/impl/XPathVisitor_232.java
+++ src/xpath/impl/XPathVisitor_232.java
@@ -1,307 +1,255 @@
 package xpath.impl;
+import org.w3c.dom.Node;
 import xpath.autogenerate.XPathBaseVisitor;
 import xpath.autogenerate.XPathParser;
+import xmlparser.XMLParser;
 
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
 public class XPathVisitor_232 extends XPathBaseVisitor<LinkedList>{
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
+    private LinkedList<Node> curNodes = new LinkedList<>();
+
     @Override
     public LinkedList visitSingle_ap(XPathParser.Single_apContext ctx) {
-
-
-        return super.visitSingle_ap(ctx);
+        XMLParser xml = new XMLParser(ctx.filename().getText());
+        this.curNodes.add(xml.getDoc());
+        this.curNodes.addAll(this.visit(ctx.rp()));
+        return this.visit(ctx.rp());
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitDouble_ap(XPathParser.Double_apContext ctx) {
-        return super.visitDouble_ap(ctx);
+        XPathTool xpathTool = XPathTool.getInstance();
+        XMLParser xml = new XMLParser(ctx.filename().getText());
+        curNodes.add(xml.getDoc());
+        curNodes.addAll(xpathTool.findAllChildren(curNodes));
+        return this.visit(ctx.rp());
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitAttname_rp(XPathParser.Attname_rpContext ctx) {
-        return super.visitAttname_rp(ctx);
+        LinkedList<Node> res = new LinkedList<>();
+        XPathTool xpathTool = XPathTool.getInstance();
+        res = xpathTool.findNodesByAttribute(curNodes, ctx.attName().getText());
+        this.curNodes = res;
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitSingle_prs(XPathParser.Single_prsContext ctx) {
-        return super.visitSingle_prs(ctx);
+        this.visit(ctx.rp(0));
+        return this.visit(ctx.rp(1));
+        // TODO: 1/27/18 按照要求这里需要进行unique(）操作，实际上我没做
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitFilter_rp(XPathParser.Filter_rpContext ctx) {
-        return super.visitFilter_rp(ctx);
+        assert this.visit(ctx.f()).size() == 1;
+        assert this.visit(ctx.f()).get(0) instanceof Boolean;
+        LinkedList<Node> res = new LinkedList<>();
+        LinkedList<Node> curNodes_bak = new LinkedList<>(this.curNodes);
+        for (Node n : curNodes_bak){
+            LinkedList<Node> curNodes_new = new LinkedList<>();
+            curNodes_new.add(n);
+            this.curNodes = curNodes_new;
+            LinkedList<Boolean> filterRet = this.visit(ctx.f());
+            assert filterRet.size() == 1;
+            assert filterRet.get(0) instanceof Boolean;
+            Boolean filterValue = filterRet.get(0);
+            if (filterValue){
+                res.add(n);
+            }
+        }
+        this.curNodes = res;
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitDouble_prs(XPathParser.Double_prsContext ctx) {
-        return super.visitDouble_prs(ctx);
+        XPathTool xpathTool = XPathTool.getInstance();
+        this.visit(ctx.rp(0));
+        this.curNodes = xpathTool.findAllChildren(curNodes);
+        // TODO: 1/27/18 这里是=还是addall，有点不确定 
+        return this.visit(ctx.rp(1));
+        // TODO: 1/27/18 按照要求这里需要进行unique(）操作，实际上我没做
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitParent_rp(XPathParser.Parent_rpContext ctx) {
-        return super.visitParent_rp(ctx);
+        XPathTool xpathTool = XPathTool.getInstance();
+        this.curNodes = xpathTool.findParents(curNodes);
+        return this.curNodes;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitSelf_rp(XPathParser.Self_rpContext ctx) {
-        return super.visitSelf_rp(ctx);
+        return this.curNodes;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitBrace_rp(XPathParser.Brace_rpContext ctx) {
-        return super.visitBrace_rp(ctx);
+        return this.visit(ctx.rp());
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitTagname_rp(XPathParser.Tagname_rpContext ctx) {
-        return super.visitTagname_rp(ctx);
+        XPathTool xpathTool = XPathTool.getInstance();
+        String tagNametoFind = ctx.getText();
+        LinkedList<Node> res = xpathTool.findNodesByTagName(curNodes, tagNametoFind);
+        this.curNodes = res;
+        return curNodes;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitChild_rp(XPathParser.Child_rpContext ctx) {
-        return super.visitChild_rp(ctx);
+        XPathTool xpathTool = XPathTool.getInstance();
+        this.curNodes = xpathTool.findNextChildren(curNodes);
+        return this.curNodes;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitValue_rp(XPathParser.Value_rpContext ctx) {
-        return super.visitValue_rp(ctx);
+        LinkedList<String> res = new LinkedList<>();
+        // TODO: 1/27/18 这里进行了类型转换，返回的不再是一个Node的List，而是一个String的List
+        LinkedList<Node> nextChildren = new LinkedList<>();
+        XPathTool xpathTool = XPathTool.getInstance();
+        nextChildren = xpathTool.findNextChildren(curNodes);
+        for (Node n : nextChildren){
+            if(n.getNodeType() == Node.TEXT_NODE){
+                // TODO: 1/27/18 有没有其它不需要输出的情况？
+                res.add(n.getNodeValue());
+            }
+        }
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitComma_rps(XPathParser.Comma_rpsContext ctx) {
-        return super.visitComma_rps(ctx);
+        LinkedList<Node> curNode_bak = (LinkedList<Node>) this.curNodes.clone();
+        LinkedHashSet<Node> res_NODUP = new LinkedHashSet<>();
+        LinkedList<Node> res = new LinkedList<>();
+        LinkedList<Node> left_res = this.visit(ctx.rp(0));
+        this.curNodes = curNode_bak;
+        // TODO: 1/27/18 这里是否需要恢复curNodes？
+        LinkedList<Node> right_res = this.visit(ctx.rp(1));
+        res_NODUP.addAll(left_res);
+        res_NODUP.addAll(right_res);
+        //Remove duplicates
+        res = new LinkedList<>(res_NODUP);
+        this.curNodes = res;
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitRp_f(XPathParser.Rp_fContext ctx) {
-        return super.visitRp_f(ctx);
+//        LinkedList<Node> curNode_bak = (LinkedList<Node>) this.curNodes.clone();
+//        LinkedList<Node> res = new LinkedList<>();
+//        for(Node n : curNode_bak){
+//            this.curNodes = new LinkedList<Node>();
+//            this.curNodes.add(n);
+//            if(this.visit(ctx.rp()).size() > 0)
+//                res.add(n);
+//            this.curNodes = curNode_bak;
+//        }
+//        this.curNodes = res;
+//        return res;
+        LinkedList<Boolean> res = new LinkedList<>();
+
+        if(this.visit(ctx.rp()).size() > 0){
+            res.add(Boolean.TRUE);
+        }else{
+            res.add(Boolean.FALSE);
+        }
+
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitAnd_fs(XPathParser.And_fsContext ctx) {
-        return super.visitAnd_fs(ctx);
+        Boolean left_res = ((LinkedList<Boolean>)this.visit(ctx.f(0))).get(0);
+        // TODO: 1/27/18 这里要加assert
+        Boolean right_res = ((LinkedList<Boolean>)this.visit(ctx.f(1))).get(0);
+        LinkedList<Boolean> res = new LinkedList<>();
+        res.add(left_res && right_res);
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitNot_f(XPathParser.Not_fContext ctx) {
-        return super.visitNot_f(ctx);
+        Boolean left_res = ((LinkedList<Boolean>)this.visit(ctx.f())).get(0);
+        // TODO: 1/27/18 这里要加assert
+        LinkedList<Boolean> res = new LinkedList<>();
+        res.add( !left_res );
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitOr_fs(XPathParser.Or_fsContext ctx) {
-        return super.visitOr_fs(ctx);
+        Boolean left_res = ((LinkedList<Boolean>)this.visit(ctx.f(0))).get(0);
+        // TODO: 1/27/18 这里要加assert
+        Boolean right_res = ((LinkedList<Boolean>)this.visit(ctx.f(1))).get(0);
+        LinkedList<Boolean> res = new LinkedList<>();
+        res.add(left_res || right_res);
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitBrace_f(XPathParser.Brace_fContext ctx) {
-        return super.visitBrace_f(ctx);
+        return this.visit(ctx.f());
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitRps_veq_f(XPathParser.Rps_veq_fContext ctx) {
-        return super.visitRps_veq_f(ctx);
+        LinkedList<Node> left_res = (LinkedList<Node>)this.visit(ctx.rp(0));
+        LinkedList<Node> right_res = (LinkedList<Node>)this.visit(ctx.rp(1));
+        LinkedList<Boolean> res = new LinkedList<>();
+        Boolean flag = false;
+        for(Node n1 : left_res){
+            flag = false;
+            for(Node n2 : right_res){
+                if(n1.isEqualNode(n2)){
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        res.add(flag);
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitRps_ieq_f(XPathParser.Rps_ieq_fContext ctx) {
-        return super.visitRps_ieq_f(ctx);
+        LinkedList<Node> left_res = (LinkedList<Node>)this.visit(ctx.rp(0));
+        LinkedList<Node> right_res = (LinkedList<Node>)this.visit(ctx.rp(1));
+        LinkedList<Boolean> res = new LinkedList<>();
+        Boolean flag = false;
+        for(Node n1 : left_res){
+            flag = false;
+            for(Node n2 : right_res){
+                if(n1 == n2){
+                    // TODO: 1/28/18 这里直接用==的方法不知道对不对
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        res.add(flag);
+        return res;
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
+    // TODO: 1/27/18 下面的应该不用写，因为上面直接用getText()取得需要的String 
     @Override
     public LinkedList visitTagName(XPathParser.TagNameContext ctx) {
         return super.visitTagName(ctx);
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitAttName(XPathParser.AttNameContext ctx) {
         return super.visitAttName(ctx);
     }
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <p>The default implementation returns the result of calling
-     * {@link #visitChildren} on {@code ctx}.</p>
-     *
-     * @param ctx
-     */
     @Override
     public LinkedList visitFilename(XPathParser.FilenameContext ctx) {
         return super.visitFilename(ctx);
