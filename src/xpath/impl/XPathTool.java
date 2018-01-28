76c2f5499296f65bde63680319ea3adfc963771b
Weiming Wu <wwm1994@gmail.com>
2018-01-28 02:10:32 -0800

    Complete the XPathTool.

--- src/xpath/impl/XPathTool.java
+++ src/xpath/impl/XPathTool.java
@@ -1,5 +1,6 @@
 package xpath.impl;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -8,17 +9,20 @@ import java.util.List;
 
 public class XPathTool {
     private static XPathTool instance = null;
+
     protected XPathTool() {
         // Exists only to defeat instantiation.
     }
+
     public static XPathTool getInstance() {
         if(instance == null) {
             instance = new XPathTool();
         }
         return instance;
     }
-    public List<Node> findNextChildren(Node startNode){
-        List<Node> res = new LinkedList<>();
+
+    public LinkedList<Node> findNextChildren(Node startNode){
+        LinkedList<Node> res = new LinkedList<>();
         NodeList children = startNode.getChildNodes();
         for(int i = 0; i < children.getLength(); i++){
             res.add(children.item(i));
@@ -26,8 +30,8 @@ public class XPathTool {
         return res;
     }
 
-    public List<Node> findNextChildren(List<Node> startNodeList){
-        List<Node> res = new LinkedList<>();
+    public LinkedList<Node> findNextChildren(List<Node> startNodeList){
+        LinkedList<Node> res = new LinkedList<>();
         for(Node node : startNodeList){
             NodeList children = node.getChildNodes();
             for(int i = 0; i < children.getLength(); i++){
@@ -37,8 +41,8 @@ public class XPathTool {
         return res;
     }
 
-    public List<Node> findAllChildren(Node startNode){
-        List<Node> res = new LinkedList<>();
+    public LinkedList<Node> findAllChildren(Node startNode){
+        LinkedList<Node> res = new LinkedList<>();
         NodeList children = startNode.getChildNodes();
         res.add(startNode);
         if(children.getLength() == 0){
@@ -49,8 +53,9 @@ public class XPathTool {
         }
         return res;
     }
-    public List<Node> findAllChildren(List<Node> startNodeList){
-        List<Node> res = new LinkedList<>();
+
+    public LinkedList<Node> findAllChildren(List<Node> startNodeList){
+        LinkedList<Node> res = new LinkedList<>();
         for(Node n : startNodeList){
             res.addAll(findAllChildren(n));
         }
@@ -60,10 +65,36 @@ public class XPathTool {
     public Node findParent(Node startNode){
         return startNode.getParentNode();
     }
-    public List<Node> findParents(List<Node> startNodeList){
-        List<Node> res = new LinkedList<>();
+
+    public LinkedList<Node> findParents(List<Node> startNodeList){
+        LinkedList<Node> res = new LinkedList<>();
         for(Node n : startNodeList){
             res.add(findParent(n));
+        }
+        return res;
+    }
+
+    public LinkedList<Node> findNodesByTagName(List<Node> nodeList, String tagNametoFind){
+        LinkedList<Node> allChildren = findAllChildren(nodeList);
+        LinkedList<Node> res = new LinkedList<>();
+        for(Node n : allChildren){
+            if(n.getNodeType() != Node.ELEMENT_NODE)
+                continue;;
+            // TODO: 1/27/18 这里是否需要加这个限制？
+            if(n.getNodeName().equals(tagNametoFind))
+                res.add(n);
+        }
+        return res;
+    }
+
+    public LinkedList<Node> findNodesByAttribute(List<Node> nodeList, String attributetoFind){
+        LinkedList<Node> allChildren = findAllChildren(nodeList);
+        LinkedList<Node> res = new LinkedList<>();
+        for(Node n : allChildren){
+            if(n.getNodeType() == Node.ELEMENT_NODE)
+                if(!((Element) n).getAttribute(attributetoFind).equals(""))
+                    res.add(n);
+
         }
         return res;
     }
